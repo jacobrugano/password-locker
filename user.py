45abf92d1d436c00332d32edd389d8a245995fa5
github_username <jacobrugano@gmail.com>
2022-02-26 09:24:38 +0300

    Created the user list array and function to handle the instances

--- user.py
+++ user.py
@@ -1,1 +1,10 @@
 class User:   #Class that generates new instances of contacts
+
+
+    user_list = [] #user_list is a Class variables(variables that belong to the entire class and can be  accessed by all instances of the class. This user_list var will be used to store our created user objects
+    def __init__(self,first_name,last_name,number,password):
+                            #  __init__ method to create new instances of a class User and to allow us to pass in properties for the new object.
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.password = password
