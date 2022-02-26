90172a19ff63bccd9478986d9725524d48568130
github_username <jacobrugano@gmail.com>
2022-02-26 09:16:19 +0300

    Added function to create a new user on the run.py

--- run.py
+++ run.py
@@ -3,3 +3,11 @@
 from traceback import print_list
 from credential import Credential
 from user import User
+
+#Creating a new user
+def create_user(first_name,last_name,number,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(first_name,last_name,number,password)
+    return new_user
