17b971f3b2f5804a611f5e3d7e67b1c222d95312
github_username <jacobrugano@gmail.com>
2022-02-26 09:31:23 +0300

    Created the function to set up the classes

--- user_test.py
+++ user_test.py
@@ -6,3 +6,9 @@ class TestUser(unittest.TestCase):
     We create a subclass class called TestUser, that inherits from unittest.TestCase and defines
     test cases for the contact class behaviours.
     '''
+
+    def setUp(self):
+        '''
+        setUp() method allows us to define instructions that will be executed before each test method.
+        '''
+        self.new_user = User("Jacob","Rugano","0711223344", "3456788") # create user object
