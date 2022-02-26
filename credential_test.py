8d1e59f4a790c08ddabf7f94a8e6fcdce332f08d
github_username <jacobrugano@gmail.com>
2022-02-26 09:46:16 +0300

    Created the credetials test class, made the imports and created the test credentials class

---
+++ credential_test.py
@@ -0,0 +1,8 @@
+import unittest # Importing the unittest module
+from credential import Credential # Importing the user class
+
+class TestCredential(unittest.TestCase):
+    '''
+    We create a subclass class called TestCredential, that inherits from unittest.TestCase and defines
+    test cases for the TestCredential class behaviours.
+    '''
