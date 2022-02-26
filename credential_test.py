2102e1d028fc6d3ce079893c54dc46dea4144c0f
github_username <jacobrugano@gmail.com>
2022-02-26 09:47:24 +0300

    Created the code to check if the Credential class has been instantiated correctly and the results are as expected.

--- credential_test.py
+++ credential_test.py
@@ -6,9 +6,17 @@ class TestCredential(unittest.TestCase):
     We create a subclass class called TestCredential, that inherits from unittest.TestCase and defines
     test cases for the TestCredential class behaviours.
     '''
-    
+
     def setUp(self):
         '''
         setUp() method allows us to define instructions that will be executed before each test method.
         '''
         self.new_credential = Credential("Jacob","3456788","0711223344") # create user object
+
+
+#Let's write code to check if the Credential class has been instantiated correctly and the results are as expected.
+    def test_init(self):
+
+        self.assertEqual(self.new_credential.user_name,"Jacob")
+        self.assertEqual(self.new_credential.pass_word,"3456788")
+        self.assertEqual(self.new_credential.phone_number,"0711223344")
