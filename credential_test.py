02045693f6c578311c8abcb82878d81c382ec7ef
github_username <jacobrugano@gmail.com>
2022-02-26 09:46:51 +0300

    Created the setUp() method allows us to define instructions that will be executed before each test method.

--- credential_test.py
+++ credential_test.py
@@ -6,3 +6,9 @@ class TestCredential(unittest.TestCase):
     We create a subclass class called TestCredential, that inherits from unittest.TestCase and defines
     test cases for the TestCredential class behaviours.
     '''
+    
+    def setUp(self):
+        '''
+        setUp() method allows us to define instructions that will be executed before each test method.
+        '''
+        self.new_credential = Credential("Jacob","3456788","0711223344") # create user object
