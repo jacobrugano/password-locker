62f01e21345a87e0ad1405bdc8ff727535238d0e
github_username <jacobrugano@gmail.com>
2022-02-26 09:43:13 +0300

    Created a create a delete_credential method that uses the remove() method to delete the credential object from the credential_list.

--- credential.py
+++ credential.py
@@ -12,3 +12,7 @@ class Credential:   #Class that generates new instances of credentials
 #We create a credential_user() method and called it on Credential object to save credentialss into the credential_list using append() method.'''  
     def save_credential(self):  
         Credential.credential_list.append(self) 
+
+#We create a delete_credential method that uses the remove() method to delete the credential object from the credential_list.
+    def delete_credential(self):
+        Credential.credential_list.remove(self)
